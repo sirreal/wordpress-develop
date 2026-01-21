c98d73ead488b28ebd191579f272c5413d8257fe
Jon Surrell <sirreal@users.noreply.github.com>
2026-01-21 12:59:12 +0100

    Add test for nested void block parsing

--- tests/phpunit/tests/block-processor/wpBlockProcessor.php
+++ tests/phpunit/tests/block-processor/wpBlockProcessor.php
@@ -1310,6 +1310,35 @@ HTML
 		);
 	}
 
+	/**
+	 * @ticket TBD
+	 */
+	public function test_nested_void_block_innerhtml_parsing() {
+		$processor = new WP_Block_Processor( '<!-- wp:wrap -->a<!-- wp:void /-->b<!-- /wp:wrap -->' );
+		assert( $processor->next_block(), 'Failed to find block in markup.' );
+		$wrapper_block = $processor->extract_full_block_and_advance();
+		$expected      = array(
+			'blockName'    => 'core/wrap',
+			'attrs'        => array(),
+			'innerBlocks'  => array(
+				array(
+					'blockName'    => 'core/void',
+					'attrs'        => array(),
+					'innerBlocks'  => array(),
+					'innerHTML'    => '',
+					'innerContent' => array(),
+				),
+			),
+			'innerHTML'    => 'ab',
+			'innerContent' => array(
+				'a',
+				null,
+				'b',
+			),
+		);
+		$this->assertSame( $expected, $wrapper_block );
+	}
+
 	//
 	// Test helpers.
 	//
