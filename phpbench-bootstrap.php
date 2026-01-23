664b3d378bcd5656d65dfab4cc15c9799b9ba6dc
Jon Surrell <sirreal@users.noreply.github.com>
2026-01-23 17:04:45 +0100

    Fix php test data constant

--- phpbench-bootstrap.php
+++ phpbench-bootstrap.php
@@ -44,7 +44,7 @@ tests_reset__SERVER();
 
 define( 'WP_TESTS_TABLE_PREFIX', $table_prefix );
 
-define( 'DIR_TESTDATA', __DIR__ . '/tests/data' );
+define( 'DIR_TESTDATA', __DIR__ . '/tests/phpunit/data' );
 define( 'WP_LANG_DIR', realpath( DIR_TESTDATA . '/languages' ) );
 
 /*
