0fb32868e56c6973da67f08ceafcc15eeb971c1d
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-12 04:10:29 +0200

    Declare WASM crate artifact

--- ext/html-api-rust/build-wasm.sh
+++ ext/html-api-rust/build-wasm.sh
@@ -3,10 +3,10 @@ set -eu
 
 cd "$(dirname "$0")"
 
-cargo rustc --target wasm32-unknown-unknown --release --lib -- --crate-type=cdylib
+cargo build --target wasm32-unknown-unknown --release --lib
 
 mkdir -p wasm/dist
-wasm_file="$(ls -t target/wasm32-unknown-unknown/release/deps/wp_html_api_rust_core-*.wasm | head -n 1)"
+wasm_file="target/wasm32-unknown-unknown/release/wp_html_api_rust_core.wasm"
 cp "$wasm_file" wasm/dist/wp_html_api_rust_core.wasm
 
 echo "WASM build complete: ext/html-api-rust/wasm/dist/wp_html_api_rust_core.wasm"
