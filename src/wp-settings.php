f9e6771d40f46f12eff485cc0e2a79bbc73fdc0a
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-12 22:20:19 +0200

    CSS Tokenizer: Add CSS token processor

--- src/wp-settings.php
+++ src/wp-settings.php
@@ -277,6 +277,8 @@ require ABSPATH . WPINC . '/html-api/class-wp-html-token.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-stack-event.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-processor-state.php';
 require ABSPATH . WPINC . '/html-api/class-wp-html-processor.php';
+require ABSPATH . WPINC . '/css-api/class-wp-css-builder.php';
+require ABSPATH . WPINC . '/css-api/class-wp-css-token-processor.php';
 require ABSPATH . WPINC . '/class-wp-block-processor.php';
 require ABSPATH . WPINC . '/html-api/css/class-wp-css-selector-parser-matcher.php';
 require ABSPATH . WPINC . '/html-api/css/class-wp-css-attribute-selector.php';
