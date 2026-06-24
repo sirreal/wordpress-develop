fb964f6bc43ee52e2165702a48975b468b98ee82
Jon Surrell <sirreal@users.noreply.github.com>
2026-06-24 09:39:07 +0200

    Tests: Run export tests in process
    Guard export headers once output has already started and define WXR helper functions only once per PHP process so the export test class no longer needs process isolation.

--- src/wp-admin/includes/export.php
+++ src/wp-admin/includes/export.php
@@ -93,9 +93,11 @@ function export_wp( $args = array() ) {
 	 */
 	$filename = apply_filters( 'export_wp_filename', $wp_filename, $sitename, $date );
 
-	header( 'Content-Description: File Transfer' );
-	header( 'Content-Disposition: attachment; filename=' . $filename );
-	header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+	if ( ! headers_sent() ) {
+		header( 'Content-Description: File Transfer' );
+		header( 'Content-Disposition: attachment; filename=' . $filename );
+		header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
+	}
 
 	if ( 'all' !== $args['content'] && post_type_exists( $args['content'] ) ) {
 		$ptype = get_post_type_object( $args['content'] );
@@ -234,266 +236,272 @@ function export_wp( $args = array() ) {
 		unset( $categories, $custom_taxonomies, $custom_terms );
 	}
 
-	/**
-	 * Wraps given string in XML CDATA tag.
-	 *
-	 * @since 2.1.0
-	 *
-	 * @param string|null $str String to wrap in XML CDATA tag. May be null.
-	 * @return string
-	 */
-	function wxr_cdata( $str ) {
-		$str = (string) $str;
+	static $wxr_functions_defined = false;
 
-		if ( ! wp_is_valid_utf8( $str ) ) {
-			$str = utf8_encode( $str );
-		}
-		// $str = ent2ncr(esc_html($str));
-		$str = '<![CDATA[' . str_replace( ']]>', ']]]]><![CDATA[>', $str ) . ']]>';
+	if ( ! $wxr_functions_defined ) {
+		/**
+		 * Wraps given string in XML CDATA tag.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param string|null $str String to wrap in XML CDATA tag. May be null.
+		 * @return string
+		 */
+		function wxr_cdata( $str ) {
+			$str = (string) $str;
 
-		return $str;
-	}
-
-	/**
-	 * Returns the URL of the site.
-	 *
-	 * @since 2.5.0
-	 *
-	 * @return string Site URL.
-	 */
-	function wxr_site_url() {
-		if ( is_multisite() ) {
-			// Multisite: the base URL.
-			return network_home_url();
-		} else {
-			// WordPress (single site): the site URL.
-			return get_bloginfo_rss( 'url' );
-		}
-	}
-
-	/**
-	 * Outputs a cat_name XML tag from a given category object.
-	 *
-	 * @since 2.1.0
-	 *
-	 * @param WP_Term $category Category Object.
-	 */
-	function wxr_cat_name( $category ) {
-		if ( empty( $category->name ) ) {
-			return;
-		}
-
-		echo '<wp:cat_name>' . wxr_cdata( $category->name ) . "</wp:cat_name>\n";
-	}
-
-	/**
-	 * Outputs a category_description XML tag from a given category object.
-	 *
-	 * @since 2.1.0
-	 *
-	 * @param WP_Term $category Category Object.
-	 */
-	function wxr_category_description( $category ) {
-		if ( empty( $category->description ) ) {
-			return;
-		}
-
-		echo '<wp:category_description>' . wxr_cdata( $category->description ) . "</wp:category_description>\n";
-	}
-
-	/**
-	 * Outputs a tag_name XML tag from a given tag object.
-	 *
-	 * @since 2.3.0
-	 *
-	 * @param WP_Term $tag Tag Object.
-	 */
-	function wxr_tag_name( $tag ) {
-		if ( empty( $tag->name ) ) {
-			return;
-		}
-
-		echo '<wp:tag_name>' . wxr_cdata( $tag->name ) . "</wp:tag_name>\n";
-	}
-
-	/**
-	 * Outputs a tag_description XML tag from a given tag object.
-	 *
-	 * @since 2.3.0
-	 *
-	 * @param WP_Term $tag Tag Object.
-	 */
-	function wxr_tag_description( $tag ) {
-		if ( empty( $tag->description ) ) {
-			return;
-		}
-
-		echo '<wp:tag_description>' . wxr_cdata( $tag->description ) . "</wp:tag_description>\n";
-	}
-
-	/**
-	 * Outputs a term_name XML tag from a given term object.
-	 *
-	 * @since 2.9.0
-	 *
-	 * @param WP_Term $term Term Object.
-	 */
-	function wxr_term_name( $term ) {
-		if ( empty( $term->name ) ) {
-			return;
-		}
-
-		echo '<wp:term_name>' . wxr_cdata( $term->name ) . "</wp:term_name>\n";
-	}
-
-	/**
-	 * Outputs a term_description XML tag from a given term object.
-	 *
-	 * @since 2.9.0
-	 *
-	 * @param WP_Term $term Term Object.
-	 */
-	function wxr_term_description( $term ) {
-		if ( empty( $term->description ) ) {
-			return;
-		}
-
-		echo "\t\t<wp:term_description>" . wxr_cdata( $term->description ) . "</wp:term_description>\n";
-	}
-
-	/**
-	 * Outputs term meta XML tags for a given term object.
-	 *
-	 * @since 4.6.0
-	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
-	 * @param WP_Term $term Term object.
-	 */
-	function wxr_term_meta( $term ) {
-		global $wpdb;
-
-		$termmeta = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $wpdb->termmeta WHERE term_id = %d", $term->term_id ) );
-
-		foreach ( $termmeta as $meta ) {
-			/**
-			 * Filters whether to selectively skip term meta used for WXR exports.
-			 *
-			 * Returning a truthy value from the filter will skip the current meta
-			 * object from being exported.
-			 *
-			 * @since 4.6.0
-			 *
-			 * @param bool   $skip     Whether to skip the current piece of term meta. Default false.
-			 * @param string $meta_key Current meta key.
-			 * @param object $meta     Current meta object.
-			 */
-			if ( ! apply_filters( 'wxr_export_skip_termmeta', false, $meta->meta_key, $meta ) ) {
-				printf( "\t\t<wp:termmeta>\n\t\t\t<wp:meta_key>%s</wp:meta_key>\n\t\t\t<wp:meta_value>%s</wp:meta_value>\n\t\t</wp:termmeta>\n", wxr_cdata( $meta->meta_key ), wxr_cdata( $meta->meta_value ) );
+			if ( ! wp_is_valid_utf8( $str ) ) {
+				$str = utf8_encode( $str );
 			}
-		}
-	}
+			// $str = ent2ncr(esc_html($str));
+			$str = '<![CDATA[' . str_replace( ']]>', ']]]]><![CDATA[>', $str ) . ']]>';
 
-	/**
-	 * Outputs list of authors with posts.
-	 *
-	 * @since 3.1.0
-	 *
-	 * @global wpdb $wpdb WordPress database abstraction object.
-	 *
-	 * @param int[] $post_ids Optional. Array of post IDs to filter the query by.
-	 */
-	function wxr_authors_list( ?array $post_ids = null ) {
-		global $wpdb;
-
-		if ( ! empty( $post_ids ) ) {
-			$post_ids       = array_map( 'absint', $post_ids );
-			$post_id_chunks = array_chunk( $post_ids, 20 );
-		} else {
-			$post_id_chunks = array( array() );
+			return $str;
 		}
 
-		$authors = array();
-
-		foreach ( $post_id_chunks as $next_posts ) {
-			$and = ! empty( $next_posts ) ? 'AND ID IN (' . implode( ', ', $next_posts ) . ')' : '';
-
-			$results = $wpdb->get_results( "SELECT DISTINCT post_author FROM $wpdb->posts WHERE post_status != 'auto-draft' $and" );
-
-			foreach ( (array) $results as $result ) {
-				$authors[] = get_userdata( $result->post_author );
+		/**
+		 * Returns the URL of the site.
+		 *
+		 * @since 2.5.0
+		 *
+		 * @return string Site URL.
+		 */
+		function wxr_site_url() {
+			if ( is_multisite() ) {
+				// Multisite: the base URL.
+				return network_home_url();
+			} else {
+				// WordPress (single site): the site URL.
+				return get_bloginfo_rss( 'url' );
 			}
 		}
 
-		$authors = array_filter( $authors );
-		$authors = array_unique( $authors, SORT_REGULAR ); // Remove duplicate authors.
+		/**
+		 * Outputs a cat_name XML tag from a given category object.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param WP_Term $category Category Object.
+		 */
+		function wxr_cat_name( $category ) {
+			if ( empty( $category->name ) ) {
+				return;
+			}
 
-		foreach ( $authors as $author ) {
-			echo "\t<wp:author>";
-			echo '<wp:author_id>' . (int) $author->ID . '</wp:author_id>';
-			echo '<wp:author_login>' . wxr_cdata( $author->user_login ) . '</wp:author_login>';
-			echo '<wp:author_email>' . wxr_cdata( $author->user_email ) . '</wp:author_email>';
-			echo '<wp:author_display_name>' . wxr_cdata( $author->display_name ) . '</wp:author_display_name>';
-			echo '<wp:author_first_name>' . wxr_cdata( $author->first_name ) . '</wp:author_first_name>';
-			echo '<wp:author_last_name>' . wxr_cdata( $author->last_name ) . '</wp:author_last_name>';
-			echo "</wp:author>\n";
-		}
-	}
-
-	/**
-	 * Outputs all navigation menu terms.
-	 *
-	 * @since 3.1.0
-	 */
-	function wxr_nav_menu_terms() {
-		$nav_menus = wp_get_nav_menus();
-		if ( empty( $nav_menus ) || ! is_array( $nav_menus ) ) {
-			return;
+			echo '<wp:cat_name>' . wxr_cdata( $category->name ) . "</wp:cat_name>\n";
 		}
 
-		foreach ( $nav_menus as $menu ) {
-			echo "\t<wp:term>";
-			echo '<wp:term_id>' . (int) $menu->term_id . '</wp:term_id>';
-			echo '<wp:term_taxonomy>nav_menu</wp:term_taxonomy>';
-			echo '<wp:term_slug>' . wxr_cdata( $menu->slug ) . '</wp:term_slug>';
-			wxr_term_name( $menu );
-			echo "</wp:term>\n";
-		}
-	}
+		/**
+		 * Outputs a category_description XML tag from a given category object.
+		 *
+		 * @since 2.1.0
+		 *
+		 * @param WP_Term $category Category Object.
+		 */
+		function wxr_category_description( $category ) {
+			if ( empty( $category->description ) ) {
+				return;
+			}
 
-	/**
-	 * Outputs list of taxonomy terms, in XML tag format, associated with a post.
-	 *
-	 * @since 2.3.0
-	 */
-	function wxr_post_taxonomy() {
-		$post = get_post();
-
-		$taxonomies = get_object_taxonomies( $post->post_type );
-		if ( empty( $taxonomies ) ) {
-			return;
+			echo '<wp:category_description>' . wxr_cdata( $category->description ) . "</wp:category_description>\n";
 		}
-		$terms = wp_get_object_terms( $post->ID, $taxonomies );
 
-		foreach ( (array) $terms as $term ) {
-			echo "\t\t<category domain=\"{$term->taxonomy}\" nicename=\"{$term->slug}\">" . wxr_cdata( $term->name ) . "</category>\n";
-		}
-	}
+		/**
+		 * Outputs a tag_name XML tag from a given tag object.
+		 *
+		 * @since 2.3.0
+		 *
+		 * @param WP_Term $tag Tag Object.
+		 */
+		function wxr_tag_name( $tag ) {
+			if ( empty( $tag->name ) ) {
+				return;
+			}
 
-	/**
-	 * Determines whether to selectively skip post meta used for WXR exports.
-	 *
-	 * @since 3.3.0
-	 *
-	 * @param bool   $return_me Whether to skip the current post meta. Default false.
-	 * @param string $meta_key  Meta key.
-	 * @return bool
-	 */
-	function wxr_filter_postmeta( $return_me, $meta_key ) {
-		if ( '_edit_lock' === $meta_key ) {
-			$return_me = true;
+			echo '<wp:tag_name>' . wxr_cdata( $tag->name ) . "</wp:tag_name>\n";
 		}
-		return $return_me;
+
+		/**
+		 * Outputs a tag_description XML tag from a given tag object.
+		 *
+		 * @since 2.3.0
+		 *
+		 * @param WP_Term $tag Tag Object.
+		 */
+		function wxr_tag_description( $tag ) {
+			if ( empty( $tag->description ) ) {
+				return;
+			}
+
+			echo '<wp:tag_description>' . wxr_cdata( $tag->description ) . "</wp:tag_description>\n";
+		}
+
+		/**
+		 * Outputs a term_name XML tag from a given term object.
+		 *
+		 * @since 2.9.0
+		 *
+		 * @param WP_Term $term Term Object.
+		 */
+		function wxr_term_name( $term ) {
+			if ( empty( $term->name ) ) {
+				return;
+			}
+
+			echo '<wp:term_name>' . wxr_cdata( $term->name ) . "</wp:term_name>\n";
+		}
+
+		/**
+		 * Outputs a term_description XML tag from a given term object.
+		 *
+		 * @since 2.9.0
+		 *
+		 * @param WP_Term $term Term Object.
+		 */
+		function wxr_term_description( $term ) {
+			if ( empty( $term->description ) ) {
+				return;
+			}
+
+			echo "\t\t<wp:term_description>" . wxr_cdata( $term->description ) . "</wp:term_description>\n";
+		}
+
+		/**
+		 * Outputs term meta XML tags for a given term object.
+		 *
+		 * @since 4.6.0
+		 *
+		 * @global wpdb $wpdb WordPress database abstraction object.
+		 *
+		 * @param WP_Term $term Term object.
+		 */
+		function wxr_term_meta( $term ) {
+			global $wpdb;
+
+			$termmeta = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM $wpdb->termmeta WHERE term_id = %d", $term->term_id ) );
+
+			foreach ( $termmeta as $meta ) {
+				/**
+				 * Filters whether to selectively skip term meta used for WXR exports.
+				 *
+				 * Returning a truthy value from the filter will skip the current meta
+				 * object from being exported.
+				 *
+				 * @since 4.6.0
+				 *
+				 * @param bool   $skip     Whether to skip the current piece of term meta. Default false.
+				 * @param string $meta_key Current meta key.
+				 * @param object $meta     Current meta object.
+				 */
+				if ( ! apply_filters( 'wxr_export_skip_termmeta', false, $meta->meta_key, $meta ) ) {
+					printf( "\t\t<wp:termmeta>\n\t\t\t<wp:meta_key>%s</wp:meta_key>\n\t\t\t<wp:meta_value>%s</wp:meta_value>\n\t\t</wp:termmeta>\n", wxr_cdata( $meta->meta_key ), wxr_cdata( $meta->meta_value ) );
+				}
+			}
+		}
+
+		/**
+		 * Outputs list of authors with posts.
+		 *
+		 * @since 3.1.0
+		 *
+		 * @global wpdb $wpdb WordPress database abstraction object.
+		 *
+		 * @param int[] $post_ids Optional. Array of post IDs to filter the query by.
+		 */
+		function wxr_authors_list( ?array $post_ids = null ) {
+			global $wpdb;
+
+			if ( ! empty( $post_ids ) ) {
+				$post_ids       = array_map( 'absint', $post_ids );
+				$post_id_chunks = array_chunk( $post_ids, 20 );
+			} else {
+				$post_id_chunks = array( array() );
+			}
+
+			$authors = array();
+
+			foreach ( $post_id_chunks as $next_posts ) {
+				$and = ! empty( $next_posts ) ? 'AND ID IN (' . implode( ', ', $next_posts ) . ')' : '';
+
+				$results = $wpdb->get_results( "SELECT DISTINCT post_author FROM $wpdb->posts WHERE post_status != 'auto-draft' $and" );
+
+				foreach ( (array) $results as $result ) {
+					$authors[] = get_userdata( $result->post_author );
+				}
+			}
+
+			$authors = array_filter( $authors );
+			$authors = array_unique( $authors, SORT_REGULAR ); // Remove duplicate authors.
+
+			foreach ( $authors as $author ) {
+				echo "\t<wp:author>";
+				echo '<wp:author_id>' . (int) $author->ID . '</wp:author_id>';
+				echo '<wp:author_login>' . wxr_cdata( $author->user_login ) . '</wp:author_login>';
+				echo '<wp:author_email>' . wxr_cdata( $author->user_email ) . '</wp:author_email>';
+				echo '<wp:author_display_name>' . wxr_cdata( $author->display_name ) . '</wp:author_display_name>';
+				echo '<wp:author_first_name>' . wxr_cdata( $author->first_name ) . '</wp:author_first_name>';
+				echo '<wp:author_last_name>' . wxr_cdata( $author->last_name ) . '</wp:author_last_name>';
+				echo "</wp:author>\n";
+			}
+		}
+
+		/**
+		 * Outputs all navigation menu terms.
+		 *
+		 * @since 3.1.0
+		 */
+		function wxr_nav_menu_terms() {
+			$nav_menus = wp_get_nav_menus();
+			if ( empty( $nav_menus ) || ! is_array( $nav_menus ) ) {
+				return;
+			}
+
+			foreach ( $nav_menus as $menu ) {
+				echo "\t<wp:term>";
+				echo '<wp:term_id>' . (int) $menu->term_id . '</wp:term_id>';
+				echo '<wp:term_taxonomy>nav_menu</wp:term_taxonomy>';
+				echo '<wp:term_slug>' . wxr_cdata( $menu->slug ) . '</wp:term_slug>';
+				wxr_term_name( $menu );
+				echo "</wp:term>\n";
+			}
+		}
+
+		/**
+		 * Outputs list of taxonomy terms, in XML tag format, associated with a post.
+		 *
+		 * @since 2.3.0
+		 */
+		function wxr_post_taxonomy() {
+			$post = get_post();
+
+			$taxonomies = get_object_taxonomies( $post->post_type );
+			if ( empty( $taxonomies ) ) {
+				return;
+			}
+			$terms = wp_get_object_terms( $post->ID, $taxonomies );
+
+			foreach ( (array) $terms as $term ) {
+				echo "\t\t<category domain=\"{$term->taxonomy}\" nicename=\"{$term->slug}\">" . wxr_cdata( $term->name ) . "</category>\n";
+			}
+		}
+
+		/**
+		 * Determines whether to selectively skip post meta used for WXR exports.
+		 *
+		 * @since 3.3.0
+		 *
+		 * @param bool   $return_me Whether to skip the current post meta. Default false.
+		 * @param string $meta_key  Meta key.
+		 * @return bool
+		 */
+		function wxr_filter_postmeta( $return_me, $meta_key ) {
+			if ( '_edit_lock' === $meta_key ) {
+				$return_me = true;
+			}
+			return $return_me;
+		}
+
+		$wxr_functions_defined = true;
 	}
 	add_filter( 'wxr_export_skip_postmeta', 'wxr_filter_postmeta', 10, 2 );
 
